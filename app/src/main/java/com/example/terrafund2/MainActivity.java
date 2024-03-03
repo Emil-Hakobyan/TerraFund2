9a08bacdbf5d4f815faff6c43d52401651d26aa7
emilhakobyan <emil.a.hakobyan@gmail.com>
2024-03-03 14:03:36 +0400

    1

---
+++ app/src/main/java/com/example/terrafund2/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.terrafund2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.terrafund2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private home homeFragment;
+    private profile profileFragment;
+    private settings settingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate fragments
+        homeFragment = new home();
+        profileFragment = new profile();
+        settingsFragment = new settings();
+
+        // Replace default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.cont, homeFragment)
+                .commit();
+
+        // Set listener for BottomNavigationView
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        if(item.getItemId() == R.id.navigation_home){
+            selectedFragment = homeFragment;
+        }
+        else if(item.getItemId() == R.id.navigation_profile){
+            selectedFragment = profileFragment;
+        } else if (item.getItemId() == R.id.navigation_settings) {
+            selectedFragment = settingsFragment;
+        }
+
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.cont, selectedFragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
+    }
+}
